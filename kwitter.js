3c9ea4a0d40eb797a45cfbce7601699e8785157f
hammibell <94499945+hammibell@users.noreply.github.com>
2022-10-09 14:05:25 -0400

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,12 @@
+function User(){
+
+    window.location("kwitter_room.html")
+    alert("Redirecting you to the kwitter rooms...hang on tight!")
+
+    document.getElementById(username).value;
+    document.getElementById(password).value;
+    localStorage.addUser("username", username);
+    localStorage.addUser("password", password);
+
+    
+}
